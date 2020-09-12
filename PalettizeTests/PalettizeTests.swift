711a499f5c686a0fd94f1e0fff841061d4c2fb06
Chris Y <christopherbcyeh@gmail.com>
2020-09-12 15:52:17 -0700

    Initial Commit

---
+++ PalettizeTests/PalettizeTests.swift
@@ -0,0 +1,34 @@
+//
+//  PalettizeTests.swift
+//  PalettizeTests
+//
+//  Created by Chris Y on 9/12/20.
+//  Copyright © 2020 Cyfr. All rights reserved.
+//
+
+import XCTest
+@testable import Palettize
+
+class PalettizeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
